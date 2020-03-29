b04a4e94f6d8244801fe5fe46ed4027cc04c1a3a
bethonxyfu <bethonxyfu@tencent.com>
2020-03-29 10:56:43 +0800

    feat: log info

--- packages/test-lerna/lib/test-lerna.js
+++ packages/test-lerna/lib/test-lerna.js
@@ -5,5 +5,5 @@ module.exports = testLerna;
 function testLerna() {
   // TODO
   const randomString = Math.random() * 100 + 1;
-  console.log("you luck number is :", randomString);
+  console.log("[info] you luck number is :", randomString);
 }

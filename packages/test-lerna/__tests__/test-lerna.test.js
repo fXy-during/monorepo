dcb02de5f11780a17c9e502b403987eb836dc4f8
bethonxyfu <bethonxyfu@tencent.com>
2020-03-29 11:13:14 +0800

    feat: github actions

--- packages/test-lerna/__tests__/test-lerna.test.js
+++ packages/test-lerna/__tests__/test-lerna.test.js
@@ -1,7 +1,13 @@
-'use strict';
+"use strict";
 
-const testLerna = require('..');
+const { add } = require("..");
 
-describe('@bethon/test-lerna', () => {
-    it('needs tests');
+describe("@bethon/test-lerna", () => {
+  it("simple add test, when given 1 and 2, got 3", () => {
+    expect(add(1, 2)).toBe(3);
+  });
+
+  it("when given 0 and 2, got 2", () => {
+    expect(add(0, 2)).toBe(2);
+  });
 });
--- packages/test-lerna/lib/test-lerna.js
+++ packages/test-lerna/lib/test-lerna.js
@@ -1,9 +1,17 @@
 "use strict";
 
-module.exports = testLerna;
+module.exports = { testLerna, add };
 
 function testLerna() {
   // TODO
   const randomString = Math.random() * 100 + 1;
   console.log("[info] you luck number is :", randomString);
+}
+
+function add(a, b) {
+  if (typeof a === "number" && typeof b === "number") {
+    return a + b;
+  } else {
+    return 0;
+  }
 }
